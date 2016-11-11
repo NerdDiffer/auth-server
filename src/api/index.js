2bba05afa794bd2c19cc5bacdc0ae70e03291e32
Rafael Espinoza <nerddiffer@gmail.com>
2016-11-10 19:49:22 -0800

    feat(api): Update API index
    Use passport, & rename API endpoints

--- src/api/index.js
+++ src/api/index.js
@@ -1,20 +1,33 @@
 const { Router } = require('express');
+const passportService = require('../services/passport');
 const corsForClient = require('./utils/cors');
 const checkUserParams = require('./utils/checkUserParams');
 const users = require('./users');
+const tokens = require('./tokens');
 
 const router = Router();
 
 router.use(corsForClient);
+
+const requireLogin = passportService.authenticate('local', {
+  session: false
+});
 
 router.get('/ping', (req, res) => {
   const msg = 'The server is up';
   res.json(msg);
 });
 
-router.post('/signup',
+router.post('/register',
   checkUserParams,
-  users.createNewUser
+  users.createNewUser,
+  tokens.sendToken
+);
+
+router.post('/login',
+  checkUserParams,
+  requireLogin,
+  tokens.sendToken
 );
 
 module.exports = router;
